695dc5126dd32bbf7360a748bd01a00a99b9ddbf
Abdul Samad <abdulsamadmughal102@gmail.com>
2025-02-17 14:23:06 +0500

    error

---
+++ Counter-APP/counter-app/src/Components/Counter.jsx
@@ -0,0 +1,14 @@
+const Profile = (props) => {
+    const [likes, setLikes] = useState(0);
+  
+    return (
+      <div>
+        <h2>{props.name}, {props.age} years old</h2>
+        <p>Likes: {likes}</p>
+        <button onClick={() => setLikes(likes + 1)}>Like ❤️</button>
+      </div>
+    );
+  };
+  
+  
+  
---
+++ Counter-APP/counter-app/src/Components/Introduction.jsx
@@ -0,0 +1,11 @@
+    import React from 'react'
+
+function Introduction({name}) {
+  return (
+    <div>
+      <h1>hello my name is {name}</h1>
+    </div>
+  )
+}
+
+export default Introduction
